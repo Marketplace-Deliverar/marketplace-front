0fc4eef6a3ba4b34f4ed0c753a63647ff8ea7b36
xMavenCalore <90734663+xMavenCalore@users.noreply.github.com>
2023-10-25 19:03:29 -0300

    lo pongo mas lindo

--- src/components/ProductCards.js
+++ src/components/ProductCards.js
@@ -5,54 +5,55 @@ import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import Button from '@mui/material/Button';
 
+const cardsData = [
+  { id: 1, label: "descripción", title: "producto 1", subheader: "precio", image: "https://thumbs.dreamstime.com/b/etiqueta-engomada-del-ejemplo-121022914.jpg" },
+  { id: 2, label: "descripción", title: "producto 2", subheader: "precio", image: "https://thumbs.dreamstime.com/b/etiqueta-engomada-del-ejemplo-121022914.jpg" },
+  { id: 3, label: "descripción", title: "producto 3", subheader: "precio", image: "https://thumbs.dreamstime.com/b/etiqueta-engomada-del-ejemplo-121022914.jpg" },
+  { id: 4, label: "descripción", title: "producto 4", subheader: "precio", image: "https://thumbs.dreamstime.com/b/etiqueta-engomada-del-ejemplo-121022914.jpg" },
+  { id: 5, label: "descripción", title: "producto 5", subheader: "precio", image: "https://thumbs.dreamstime.com/b/etiqueta-engomada-del-ejemplo-121022914.jpg" },
+  { id: 6, label: "descripción", title: "producto 6", subheader: "precio", image: "https://thumbs.dreamstime.com/b/etiqueta-engomada-del-ejemplo-121022914.jpg" },
+  { id: 7, label: "descripción", title: "producto 7", subheader: "precio", image: "https://thumbs.dreamstime.com/b/etiqueta-engomada-del-ejemplo-121022914.jpg" },
+  { id: 8, label: "descripción", title: "producto 8", subheader: "precio", image: "https://thumbs.dreamstime.com/b/etiqueta-engomada-del-ejemplo-121022914.jpg" },
+  { id: 9, label: "descripción", title: "producto 9", subheader: "precio", image: "https://thumbs.dreamstime.com/b/etiqueta-engomada-del-ejemplo-121022914.jpg" },
+];
 
 export default function ProductCards() {
- 
   return (
-    <div onClick={() => alert("Hello from here")}>
-    <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Producto1"
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <ShoppingCartIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
+    <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+      {cardsData.map((card) => (
+        <Card key={card.id} sx={{ width: 345, height: 400, margin: '16px' }}>
+          <CardHeader
+            title={card.title}
+            subheader={card.subheader}
+          />
+          <CardMedia
+            component="img"
+            height="194"
+            image={card.image}
+            alt={card.title}
+          />
+          <CardContent>
+            <Typography variant="body2" color="text.secondary">
+              {card.label}
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <ShoppingCartIcon />
+            </IconButton>
+            <IconButton aria-label="share">
+              <ShareIcon />
+            </IconButton>
+          </CardActions>
+        </Card>
+      ))}
     </div>
   );
 }
