f398135294db57fa0ad55ca0a0ec2047651121e9
maxparisiuade <144498369+maxparisiuade@users.noreply.github.com>
2023-10-26 20:59:54 -0300

    Update usuariosController.js

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -1,22 +1,26 @@
-import urlWebServices from './webserviceController';
+import urlWebServices from "./webserviceController";
 
 export const obtenerDatosUsuario = async function (uId) {
     const url = urlWebServices.obtenerDatosUsuario + `${uId}`; // Agrega el ID del usuario a la URL
     try {
         const response = await fetch(url, {
-            method: 'GET',
-            mode: 'cors',
+            method: "GET",
+            mode: "cors",
             headers: {
-                'Origin': 'https://xorn7asoxb4eecmwmszz5fbc3a0wamui.lambda-url.us-east-1.on.aws/'
-            }
+                Origin:
+                    "https://xorn7asoxb4eecmwmszz5fbc3a0wamui.lambda-url.us-east-1.on.aws/",
+            },
         });
 
         if (response.status === 200) {
             const data = await response.json();
-            console.log("obtengo data?:", data)
+            console.log("obtengo data?:", data);
             return data;
         } else {
-            console.error("Error al obtener datos del usuario. Código de estado:", response.status);
+            console.error(
+                "Error al obtener datos del usuario. Código de estado:",
+                response.status
+            );
             return null; // Devuelve null en caso de error
         }
     } catch (error) {
@@ -26,17 +30,18 @@ export const obtenerDatosUsuario = async function (uId) {
 };
 
 export const editarDatosUsuario = async function (userId, userData) {
-    const url = urlWebServices.editarDatosUsuario +`${userId}`; // Debes incluir el userId en la URL
-    console.log(userData)
+    const url = urlWebServices.editarDatosUsuario + `${userId}`; // Debes incluir el userId en la URL
+    console.log("UserData:", userData);
     try {
         let response = await fetch(url, {
-            method: 'PUT',
-            mode: 'cors',
+            method: "PUT",
+            mode: "cors",
             headers: {
-                'Origin': 'https://xorn7asoxb4eecmwmszz5fbc3a0wamui.lambda-url.us-east-1.on.aws/',
+                Origin:
+                    "https://xorn7asoxb4eecmwmszz5fbc3a0wamui.lambda-url.us-east-1.on.aws/",
+                "Content-Type": "application/json",
             },
-            body: userData,
-            
+            body: JSON.stringify(userData),
         });
 
         let rdo = response.status;
@@ -59,4 +64,4 @@ export const editarDatosUsuario = async function (userId, userData) {
         console.error("Error", error);
         // Puedes manejar el error de una manera más adecuada según tus necesidades
     }
-}
+};
