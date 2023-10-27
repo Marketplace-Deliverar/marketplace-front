1ac0a61c7a41a675c9155dd71dd723c50e261d41
Flor <florenciacuccarese@gmail.com>
2023-10-27 03:00:23 -0300

    agregar productos funciona

--- src/controllers/productoController.js
+++ src/controllers/productoController.js
@@ -26,3 +26,59 @@ export const obtenerProductosEmpresa = async function (id) {
         throw new Error(`Error al obtener empresas: ${error}`);
     };
 }
+
+export const obtenerCategoriasRubro= async function (Rubro) {
+    let url = urlWebServices.obtenerCategoriasPorRubro + Rubro + "/categoria";
+    try {
+        let response = await fetch(url, {
+            method: 'GET',
+            mode: "cors",
+            headers: {
+                'Origin': 'https://xorn7asoxb4eecmwmszz5fbc3a0wamui.lambda-url.us-east-1.on.aws/'
+            }
+        });
+        if (response.status === 200) {
+            let data = await response.json();
+            let Categorias = data
+            console.log(data)
+            return Categorias;
+        }
+        else {
+            console.error("Error al obtener categorias. Código de estado:", response.status);
+            return [];
+        }
+
+    }
+    catch (error) {
+        throw new Error(`Error al obtener categorias: ${error}`);
+    };
+}
+
+export const crearProducto = async function (ProductoData) {
+    let url = urlWebServices.Productos
+    try {
+        let response = await fetch(url, {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json', 
+                'Origin': 'https://xorn7asoxb4eecmwmszz5fbc3a0wamui.lambda-url.us-east-1.on.aws/'
+            },
+            body: JSON.stringify(ProductoData) 
+        });
+        console.log(response)
+        console.log(JSON.stringify(ProductoData))
+
+        if (response.status === 201) {
+            let data = await response.json();
+            let datosProducto = data;
+            console.log(data);
+            return datosProducto;
+        } else {
+            console.error("Error al actualizar empresa. Código de estado:", response.status);
+            return null; 
+        }
+    } catch (error) {
+        throw new Error(`Error al actualizar empresa: ${error}`);
+    }
+}
