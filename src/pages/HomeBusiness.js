4906cd63554b32bd6f71e59d5e05b2d385eb2057
maxparisiuade <144498369+maxparisiuade@users.noreply.github.com>
2023-11-20 20:09:47 -0300

    Agregado Modal

--- src/pages/HomeBusiness.js
+++ src/pages/HomeBusiness.js
@@ -1,13 +1,18 @@
-import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
-import ProductCards from "../components/ProductCards";
-import BusinessImage from "../components/BusinessImage";
-import { LeftSidebarFilter } from "../leftSidebarFilter";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import ProductCards from '../components/ProductCards';
+import BusinessImage from '../components/BusinessImage';
+import { LeftSidebarFilter } from '../leftSidebarFilter';
 
 export default function HomeBusiness({ empresaURL, setNavBarColor }) {
 
   const [empresa, setEmpresa] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
 
   const obtenerDatosEmpresa = async (dominio) => {
     try {
@@ -34,7 +39,14 @@ export default function HomeBusiness({ empresaURL, setNavBarColor }) {
   };
 
   useEffect(() => {
-    // Realizar la solicitud para obtener los datos de la empresa
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const successParam = urlSearchParams.get('success');
+    const isSuccess = successParam === 'true';
+
+    if (isSuccess) {
+      setOpenModal(true);
+    }
+
     obtenerDatosEmpresa(empresaURL)
       .then((datos) => {
         setEmpresa(datos);
@@ -44,6 +56,10 @@ export default function HomeBusiness({ empresaURL, setNavBarColor }) {
       });
   }, [empresaURL]);
 
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
   return (
     <div>
       <BusinessImage empresa={empresa?.razon_social} setNavBar={setNavBarColor} />
@@ -51,6 +67,34 @@ export default function HomeBusiness({ empresaURL, setNavBarColor }) {
         <LeftSidebarFilter />
         <ProductCards empresa={empresa} />
       </Stack>
+
+      <Modal open={openModal} onClose={handleCloseModal}>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 400,
+            bgcolor: 'white',
+            border: '2px solid #000',
+            boxShadow: 24,
+            p: 4,
+            borderRadius: '8px',
+          }}
+        >
+          <CheckCircleOutlineIcon color="success" sx={{ fontSize: 50, mb: 2 }} />
+          <Typography variant="h5" mb={2}>
+            ¡Gracias por elegirnos!
+          </Typography>
+          <Typography variant="body1">
+            La factura estará disponible pronto en la sección 'Mis Pedidos'.
+          </Typography>
+          <Button onClick={handleCloseModal} variant="contained" color="success" sx={{ mt: 3 }}>
+            Cerrar
+          </Button>
+        </Box>
+      </Modal>
     </div>
   );
 }
